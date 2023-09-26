b370a26010361f4101bf2aef05fc1c599d4022dc
Yujin <jin37091289@gmail.com>
2023-09-26 17:28:03 +0900

    feat : Board 클래스 생성

---
+++ src/main/java/com/project/insert/domain/board/Board.java
@@ -0,0 +1,28 @@
+package com.project.insert.domain.board;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Lob
+    private String content;
+
+    @Column(name = "date")
+    private java.sql.Date date;
+
+
+}
