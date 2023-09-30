8a28a6c3895de1d46419a00d78c701e21fb7aed5
jyj1289 <jin37091289@gmail.com>
2023-10-01 05:39:54 +0900

    feat : read all api 컨트롤러

--- src/main/java/com/project/insert/presentation/post/PostController.java
+++ src/main/java/com/project/insert/presentation/post/PostController.java
@@ -1,14 +1,18 @@
 package com.project.insert.presentation.post;
 
+import com.project.insert.domain.post.Post;
 import com.project.insert.domain.post.service.PostService;
 import com.project.insert.presentation.post.dto.PostDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,6 +23,14 @@ public class PostController {
     @PostMapping("/api/create")
     public void createPost(@RequestPart("requestDto") PostDto postDto,
                            @RequestPart(value = "files", required = false) List<MultipartFile> files) throws IOException{
-        postService.createPost(postDto, files);
+        List<String> types = new ArrayList<>();
+        types.add("thumbnail");
+        types.add("normal");
+        postService.createPost(postDto, files, types);
+    }
+
+    @GetMapping("/api/read/all")
+    public List<PostDto> readAll() {
+        return postService.readAll();
     }
 }
