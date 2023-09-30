b4edf0bcab5dba50844782858f360dffd3e60bd8
J1W0N-1209 <nabook754@naver.com>
2023-10-01 00:31:58 +0900

    fix : Token 재발급 할 때 exp null 에러 해결

--- src/main/java/com/project/insert/global/jwt/util/JwtProvider.java
+++ src/main/java/com/project/insert/global/jwt/util/JwtProvider.java
@@ -10,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.time.ZonedDateTime;
+import java.util.Date;
+
 import static com.project.insert.global.jwt.config.JwtConstants.*;
 
 @RequiredArgsConstructor
@@ -30,19 +32,22 @@ public class JwtProvider {
                 .id(authId)
                 .refreshToken(refreshToken)
                 .ttl(jwtProperties.getRefreshExp() * 1000)
+                .expiredAt(getExpiredTime())
                 .build()
         );
 
         return new TokenResponseDto(accessToken, refreshToken, getExpiredTime());
     }
 
-    private String generateToken(String authId,String role,String type,Long time) {
+    private String generateToken(String authId,String role,String type,Long exp) {
         return Jwts.builder()
-                .setSubject(authId)
-                .setHeaderParam(TYPE.message,type)
-                .claim(AUTH_ID.message, authId)
-                .claim(ROLE.message, role)
+                .setHeaderParam(TYPE.message, type)
+                .claim(ROLE.getMessage(), role)
+                .claim(AUTH_ID.getMessage(), authId)
                 .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecret())
+                .setExpiration(
+                        new Date(System.currentTimeMillis() + exp * 1000)
+                )
                 .compact();
     }
 
