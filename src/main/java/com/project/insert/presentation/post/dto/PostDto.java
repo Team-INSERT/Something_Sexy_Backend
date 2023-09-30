4fe5a32a963de108b760b4f36f8e2e902834a324
jyj1289 <jin37091289@gmail.com>
2023-10-01 05:40:40 +0900

    feat : 썸네일 경로 받기

--- src/main/java/com/project/insert/presentation/post/dto/PostDto.java
+++ src/main/java/com/project/insert/presentation/post/dto/PostDto.java
@@ -1,13 +1,16 @@
 package com.project.insert.presentation.post.dto;
 
+import com.project.insert.domain.post.Image;
 import com.project.insert.domain.post.Post;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @NoArgsConstructor
 @Getter
+@Setter
 public class PostDto {
 
     private Long id;
@@ -17,11 +20,14 @@ public class PostDto {
     private String content;
 
     private LocalDate localDate;
+    private String thumbnailUrl;
 
-    public PostDto(Post post){
+
+    public PostDto(Post post, Image thumbnailUrl){
         this.id = post.getId();
         this.title = post.getTitle();
         this.content = post.getContent();
         this.localDate = post.getLocalDate();
+        this.thumbnailUrl = thumbnailUrl.getUrl();
     }
 }
