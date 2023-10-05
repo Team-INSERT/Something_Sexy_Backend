da737bbd0c89a7427e7d6f72fe40469ca4c1a9d3
Yujin <jin37091289@gmail.com>
2023-10-05 16:45:06 +0900

    chore : gradle 수정

--- src/main/java/com/project/insert/InsertApplication.java
+++ src/main/java/com/project/insert/InsertApplication.java
@@ -2,10 +2,10 @@ package com.project.insert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@EnableJpaAuditing
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class InsertApplication {
 
 	public static void main(String[] args) {
